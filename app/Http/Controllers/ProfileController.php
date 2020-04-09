27a2547a5220d9286e1d921a2f80d4b2f6a1a2a6
chess254 <lae2006a@yahoo.com>
2020-04-09 12:38:51 +0300

    minor tweaks on profilecontroller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,33 +9,18 @@ use App\SeekerProfile;
 class ProfileController extends Controller
 {
     public function index(){
-
         $profiles = SeekerProfile::all();
-
         dd($profiles);
         return view('profile.index', ['profiles']);
     }
 
     public function show($user_id){
-
-        // $id = auth()->
-        // $profile = null;
-        // if(auth()->check()){
         $profile = SeekerProfile::where('user_id', $user_id)->first();
-        // }
-        // dd($profile->experienceDetails);
-        // dd($profile->user_id);
-
         return view('profile.show', compact('profile'));
     }
 
     public function update(Request $request, $user_id){
         $profile = SeekerProfile::where('user_id', $user_id)->first();
-
-      
-        // dd(auth()->user()->id);
-        // dd($request->educationDetails_id);
-
 
         if($request->edit_bio){
             $profile->bio = $request->edit_bio;
@@ -43,7 +28,6 @@ class ProfileController extends Controller
         $profile->save();
 
         if($request->educationDetails_id){
-
             if(auth()->user()->id == $profile->user_id){
                 $educationDetails = $profile->educationDetails->find($request->educationDetails_id);
                 $educationDetails->institute = $request->edit_institute;
@@ -54,9 +38,6 @@ class ProfileController extends Controller
                 $educationDetails->save();
             }
         }
-        
-        // dd($request->all());
-    return redirect()->back();
-        // return redirect('/profile/'.$user_id);
+        return redirect()->back();    
     }
 }
