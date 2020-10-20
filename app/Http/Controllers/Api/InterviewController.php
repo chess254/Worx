fc61e256a4ed283c07a83f9591fb1779f48de794
chess254 <lae2006a@yahoo.com>
2020-10-20 15:21:03 +0300

    initial interview generation from application start  time and end time

--- app/Http/Controllers/Api/InterviewController.php
+++ app/Http/Controllers/Api/InterviewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\EducationDetails;
+use App\Interview;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -20,13 +21,31 @@ class InterviewController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
+     * 
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        if($request->application_id){
+            $application = Application::find($request->application_id)->first();
+            $interview = new Interview();
+            $interview->job_id = $application->job_id;
+            $interview->application_id = $application->id;
+            $interview->company_id = $application->company_id;
+            $interview->save();
+            $interview->seeker_profiles()->attach($application->applicant_id,['start_time'=> new DateTime('now') , 'end_time'=> new DateTime('now') , 'status'=> 'shortlisted'])
+        }
     }
+
+    // public static function makeFromApplication(Application $application): self
+    // {
+    //     // $application = Application::find($request->application_id)->first();
+    //     return self::make([
+    //         'job_id' => $applicatio->job_id,
+    //         'application_id' => $application->id,
+    //         'company_id' => $application->company_id,
+    //     ]);
+    // }
 
     /**
      * Store a newly created resource in storage.
