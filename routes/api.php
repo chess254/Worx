1f7ce8eddcb5efa5fec548a28a78a9004468f2e1
chess254 <lae2006a@yahoo.com>
2021-12-16 18:21:39 +0300

    refactor

--- routes/api.php
+++ routes/api.php
@@ -9,16 +9,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\JobController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   $user =User::where('id',auth()->user()->id);
